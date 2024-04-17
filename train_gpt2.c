1c74d15cc6a3090adf740967bb6e8903bf2c7c4e
Coenraad <coenraad@wish.org.za>
2024-04-18 00:08:46 +0200

    Gain 10% on CPU performance on gcc by moving -fno-finite-math-only to gelu_backwards only

--- train_gpt2.c
+++ train_gpt2.c
@@ -363,6 +363,9 @@ void gelu_forward(float* out, float* inp, int N) {
     }
 }
 
+#ifdef __GNUC__
+  __attribute__((optimize("no-finite-math-only"))) 
+#endif
 void gelu_backward(float* dinp, float* inp, float* dout, int N) {
     for (int i = 0; i < N; i++) {
         float x = inp[i];
